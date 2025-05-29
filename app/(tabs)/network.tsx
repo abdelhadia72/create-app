ae3ec592ab5e69fac279c8553ffed35577b29d1d
abdelhadia72 <thisisavnb@gmail.com>
2025-05-29 17:47:50 +0100

    setup

---
+++ app/(tabs)/network.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Newwork = () => {
+  return (
+    <View className='p-20 bg-red-400'>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default Newwork;
